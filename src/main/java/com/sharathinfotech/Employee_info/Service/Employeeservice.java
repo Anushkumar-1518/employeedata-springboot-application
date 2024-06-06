8d6a7df4f7974c8529fcfeb0597de18a8d3be079
Anushkumar-1518 <126445659+Anushkumar-1518@users.noreply.github.com>
2024-06-06 13:35:43 +0530

    Update Employeeservice.java

--- src/main/java/com/sharathinfotech/Employee_info/Service/Employeeservice.java
+++ src/main/java/com/sharathinfotech/Employee_info/Service/Employeeservice.java
@@ -5,11 +5,26 @@ import com.sharathinfotech.Employee_info.Repository.Employeerepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 @Service
 public class Employeeservice {
     @Autowired
     private Employeerepository repo;
     public Employee addEmployee(Employee employee) {
         return repo.save(employee);
+    }
+
+    public List<Employee> getEmployee() {
+        return repo.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(int id) {
+        return repo.findById(id);
+    }
+
+    public String deleteEmployeeById(int id) {
+        repo.deleteById(id);
+        return " employee is Deleted";
     }
 }

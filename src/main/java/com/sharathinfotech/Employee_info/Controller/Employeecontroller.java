6689e920fa3caf51d19c822e113d0a7c7c5f8097
Anushkumar-1518 <126445659+Anushkumar-1518@users.noreply.github.com>
2024-06-06 13:34:24 +0530

    Update Employeecontroller.java

--- src/main/java/com/sharathinfotech/Employee_info/Controller/Employeecontroller.java
+++ src/main/java/com/sharathinfotech/Employee_info/Controller/Employeecontroller.java
@@ -3,22 +3,34 @@ package com.sharathinfotech.Employee_info.Controller;
 import com.sharathinfotech.Employee_info.Model.Employee;
 import com.sharathinfotech.Employee_info.Service.Employeeservice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/employee")
 public class Employeecontroller {
+    @Autowired
+    private Employeeservice service;
 
-
-        @Autowired
-        private Employeeservice service;
-        @PostMapping("/addemp")
-                public Employee addEmployee(@RequestBody Employee employee){
-            return service.addEmployee(employee);
-        }
+    @PostMapping("/addemp")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return service.addEmployee(employee);
     }
+    @GetMapping
+    public List<Employee> getEmployee(){
+        return (List<Employee>) service.getEmployee();
+    }
+    @GetMapping("/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable int id){
+        return service.getEmployeeById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteEmployeeById(@PathVariable int id){
+        return service.deleteEmployeeById(id);
+    }
+
+}
 
 
